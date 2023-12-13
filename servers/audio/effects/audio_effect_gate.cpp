b465c7e8aa0ce3b9aeb6469019714101814a5dd7
Marc <marc.weber54@gmail.com>
2023-12-12 19:00:14 -0700

    Attempt to implement the gate process function

--- servers/audio/effects/audio_effect_gate.cpp
+++ servers/audio/effects/audio_effect_gate.cpp
@@ -2,12 +2,56 @@
 
 #include "audio_effect_gate.h"
 
+#include "core/math/math_funcs.h"
+#include "servers/audio_server.h"
+
 void AudioEffectGateInstance::process(const AudioFrame *p_src_frames, AudioFrame *p_dst_frames, int p_frame_count) {
     
-    // Just write silence for now
+    float mix_rate = AudioServer::get_singleton()->get_mix_rate();
+
+    float signal_total_l_sqr = 0.f;
+    float signal_total_r_sqr  = 0.f;
     for(int i=0; i<p_frame_count; i++){
-        p_dst_frames[i] = AudioFrame(0.f, 0.f);
+        signal_total_l_sqr += p_src_frames[i].l * p_src_frames[i].l;
+        signal_total_r_sqr += p_src_frames[i].r * p_src_frames[i].r;
     }
+
+    float DB_RMS_L = Math::sqrt(signal_total_l_sqr / p_frame_count);
+    float DB_RMS_R = Math::sqrt(signal_total_r_sqr / p_frame_count);
+
+    float DB_RMS = DB_RMS_L > DB_RMS_R ? DB_RMS_L : DB_RMS_R; // Use the larger one
+
+    bool now_below_threshhold = DB_RMS < base->threshhold_db;
+    if(now_below_threshhold && !below_threshhold){
+        // Start ducking envelope
+
+        for(int i=0; i<p_frame_count; i++){
+            p_dst_frames[i] = p_src_frames[i] * (1.f - ( 1.f * i / p_frame_count));
+        }
+    }
+    else if(!now_below_threshhold && below_threshhold){
+        // End ducking envelope
+
+        for(int i=0; i<p_frame_count; i++){
+            p_dst_frames[i] = p_src_frames[i] * ( 1.f * i / p_frame_count);
+        }
+    }
+    else if(now_below_threshhold){
+        // Still ducking, output silence
+
+        for(int i=0; i<p_frame_count; i++){
+            p_dst_frames[i] = AudioFrame(0.f, 0.f);
+        }
+    }
+    else if(!now_below_threshhold){
+        // Still not ducking, forward the signal
+
+        for(int i=0; i<p_frame_count; i++){
+            p_dst_frames[i] = p_src_frames[i];
+        }
+    }
+
+    below_threshhold = now_below_threshhold;
 }
 
 Ref<AudioEffectInstance> AudioEffectGate::instantiate() {
--- servers/audio/effects/audio_effect_gate.h
+++ servers/audio/effects/audio_effect_gate.h
@@ -1,5 +1,4 @@
 
-
 #ifndef AUDIO_EFFECT_GATE_H
 #define AUDIO_EFFECT_GATE_H
 
@@ -12,6 +11,8 @@ class AudioEffectGateInstance : public AudioEffectInstance {
 	friend class AudioEffectGate;
 
 	Ref<AudioEffectGate> base;
+
+    bool below_threshhold = false;
 
 public:
 	virtual void process(const AudioFrame *p_src_frames, AudioFrame *p_dst_frames, int p_frame_count) override;
